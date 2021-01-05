9ea086ff54dc0b436088ef75dbdc228c5ab17cfa
igorgo-hyuna <igor.gologots@hyuna.bb>
2021-01-05 17:00:38 +0200

    Clean version 2.0. Minimized files

--- app/js/common.js
+++ app/js/common.js
@@ -15,55 +15,10 @@ $(document).ready(function() {
         if ($(".top_mnu").is(":visible")) {
             $(".top_text").css("opacity", "1");
             $(".top_mnu").fadeOut(600);
-            $(".top_mnu li a").removeClass("fadeInUp animated");
         } else {
             $(".top_text").css("opacity", ".1");
             $(".top_mnu").fadeIn(600);
-            $(".top_mnu li a").addClass("fadeInUp animated");
         };
     });
 
-    /* Parallax 
-    $('.parallax-window').parallax({imageSrc: 'img/background.jpg'}); */
-
-    /* Resize
-    function heightDetect(){
-        $(".parallax-window").css("height", $(window).height());    
-    }
-    heightDetect();
-    $(window).resize(function(){
-        heightDetect();
-    });
-     */
-
-    /* Scrool animation
-    $(window).scroll(function() {
-
-        $('.mov').each(function() {
-            var imagePos = $(this).offset().top;
-            var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow + 600) {
-                $(this).addClass('fadeIn animated');
-                $(this).css('opacity', '1');
-            }
-        });
-
-        $('.mov-left').each(function() {
-            var imagePos = $(this).offset().top;
-            var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow + 600) {
-                $(this).addClass('fadeInLeft animated');
-                $(this).css('opacity', '1');
-            }
-        });
-
-        $('.mov-right').each(function() {
-            var imagePos = $(this).offset().top;
-            var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow + 600) {
-                $(this).addClass('fadeInRight animated');
-                $(this).css('opacity', '1');
-            }
-        });
-    });*/
 });
